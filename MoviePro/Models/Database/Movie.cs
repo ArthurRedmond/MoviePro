1615ce473c018c737d1370317d9c25efdff6e822
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-03-22 16:27:59 -0400

    Creation of database classes

--- MoviePro/Models/Database/Movie.cs
+++ MoviePro/Models/Database/Movie.cs
@@ -42,6 +42,11 @@ namespace MoviePro.Models.Database
         [NotMapped]
         [Display(Name = "Backdrop Image")]
         public IFormFile BackdropFile { get; set; }
+
+        //Navigation
+        public ICollection<MovieCollection> Collections { get; set; } = new HashSet<MovieCollection>();
+        public ICollection<MovieCast> Cast { get; set; } = new HashSet<MovieCast>();
+        public ICollection<MovieCrew> Crew { get; set; } = new HashSet<MovieCrew>();
     }
 
 }
